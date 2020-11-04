6fa447b3e63dca22d79a7b008da8298c71177ea6
LuisTaborda <luisgustavo.taborda@gmail.com>
2020-11-04 15:11:24 -0300

    adicionando insert

--- pos-java-jbdc/src/test/java/pos_java_jbdc/pos_java_jbdc/TesteBancoJdbc.java
+++ pos-java-jbdc/src/test/java/pos_java_jbdc/pos_java_jbdc/TesteBancoJdbc.java
@@ -3,11 +3,21 @@ package pos_java_jbdc.pos_java_jbdc;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserPosDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc {
 	
 	@Test
 	public void initBanco() {
-		SingleConnection.getConnection();
+//		SingleConnection.getConnection();
+		UserPosDAO userPosDAO = new UserPosDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(2l);
+		userposjava.setNome("Teste");
+		userposjava.setEmail("teste@testes.com.br");
+		
+		userPosDAO.salvar(userposjava);
 	}
 }
